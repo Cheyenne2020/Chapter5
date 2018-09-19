380ffc8cfde744c8a60c374f87f6fc898de7bc27
Cheyenne Jones <cj1011012020@gmail.com>
2018-09-19 09:46:15 -0500

    Added Class and changed code

--- Chapter5/src/Practice/DoorGame.java
+++ Chapter5/src/Practice/DoorGame.java
@@ -5,6 +5,8 @@ public class DoorGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int doorChoice;
+		int chestChoice;
+		int bookChoice;
 		String result;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to the amazing door choice game!");
@@ -14,7 +16,22 @@ public class DoorGame {
 		
 		if(doorChoice == 1)
 		{
-			result = "a pile of gold"; 
+			System.out.println("Door 1 good choice choose amoung the following Chests");
+			System.out.println("1-For Chest One\n2- For Chest Two");
+			chestChoice = input.nextInt();
+			if(chestChoice == 1) 
+			{
+			result = "a pile of gold";
+			}
+			else if(chestChoice == 2) 
+			{
+				result = "Empty HAHAHA";
+			}
+			else 
+			{
+				result = "just kidding, I said 1 or 2!, now die!";
+			}
+				
 		}
 		else if(doorChoice == 2)
 		{
@@ -23,6 +40,27 @@ public class DoorGame {
 		else if(doorChoice == 3)
 		{
 			result = " a long fast food line!"; 
+		}
+		else if(doorChoice == 3) {
+			System.out.println("You enter a special room with 3 books on the table, there are no +"
+					+ "words on the cover. Do you choose Book 1, Book 2, Book 3?");
+			bookChoice = input.nextInt();
+			if(bookChoice == 1) 
+			{
+			result = "You die ";
+			}
+			else if(bookChoice ==2) 
+			{
+				result = "you somewhat die";
+			}
+			else if(bookChoice ==3) 
+			{
+				result = "You live";
+			}
+			else {
+				result = " You should have picked a book!"; 
+			}
+				
 		}
 		else
 		{
