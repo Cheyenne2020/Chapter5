95ea04221393a780850bed3f6f39eb06475de3f5
Cheyenne Jones <cj1011012020@gmail.com>
2018-09-26 08:56:44 -0500

    added code

--- Chapter5/src/Excercises/CellPhoneService.java
+++ Chapter5/src/Excercises/CellPhoneService.java
@@ -6,27 +6,39 @@ public class CellPhoneService {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int minuets;
+		int minutes;
 		int txtMessages;
 		int dataUsage;
-		String result;
+		String result = null;
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Please enter monthly talk minuets used");
-		minuets = input.nextInt();
+		minutes = input.nextInt();
 		System.out.println("Please enter monthly text messages sent");
 		txtMessages = input.nextInt();
 		System.out.println("Please enter monthly data used");
 		dataUsage = input.nextInt();
 		
-		if(minuets < 500 && dataUsage == 0 && txtMessages == 0) {
+		if(minutes > 500 && dataUsage == 0 && txtMessages == 0) {
 			result = "You need plan A at $49 per month";
 		}
-		else if(minuets < 500 && txtMessages >= 1) {
+		else if(minutes < 500 && txtMessages > 0 && dataUsage == 0) {
 			result = "You need plan B at $55 per month";
 		}
-		else if()
-
+		else if(minutes > 500 && dataUsage == 0 ) {
+			if(txtMessages < 100) {
+				result = "You need plan C at $61 per month";
+			} 
+			else 
+				result = "You need plan D at $70 per month";
+		}
+		else if(dataUsage > 0 && dataUsage < 2) {
+			result = "You need plan E at $79 per month";
+		}
+		else if( dataUsage >= 2) {
+			result = "You need plan F at $87 per month";
+		}
+		System.out.println(result);
 	}
 
 }
